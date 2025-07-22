14e2122096587a47881473b7ab2da2c9a3576ea5
Vladislav Grechannik <vgrechannik@gmail.com>
2025-07-22 15:21:56 +0200

    Add mozlz4

---
+++ pkgs/mozlz4/default.nix
@@ -0,0 +1,27 @@
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+}:
+rustPlatform.buildRustPackage rec {
+  pname = "mozlz4";
+  version = "0.1.0";
+
+  src = fetchFromGitHub {
+    owner = "jusw85";
+    repo = pname;
+    rev = "d003163712347b2aa5e88396281ee0cb511d4638";
+    hash = "sha256-csLtrErA0miCPMJMOXrDGH9RlszpZPyPSaA+zToaEzQ=";
+  };
+
+  cargoLock = {
+    lockFile = "${src}/Cargo.lock";
+  };
+
+  meta = with lib; {
+    description = "Decompress / compress mozlz4 files";
+    homepage = "https://github.com/jusw85/${pname}";
+    license = licenses.mit;
+    maintainers = with maintainers; [ vladexa ];
+  };
+}
